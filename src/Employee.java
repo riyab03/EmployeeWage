cf20d9cb901846b0518287430a60c9af79f8e507
rbisht03 <riyabisht979@gmail.com>
2024-12-01 14:20:16 +0530

    Added:Monthly Wage for multiple companies

--- src/Employee.java
+++ src/Employee.java
@@ -1,18 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Employee {
+    Scanner sc=new Scanner(System.in);
+    //Employee name
     String name;
+    //Employee id
     int id;
+    //Employee attendance: true/false
     boolean attendance;
+    //number of daily work day
     int normalWorkday=0;
+    //Salary per Hour
     int salperhour=20;
+    //Daily Salary
     double dailySalary=0;
+    //number of working days per month
     int monthworkday=20;
 //    double MonthlySalary=0;
+    //total working hours
     int maxHours=100;
+
     int workingHours=0;
     int wageTillCon=0;
     int totalWage=0;
     int totalHours=0;
 
+    //method for taking input : name,id,sal per hr,working days/month,total working days
+    public void input(){
+        System.out.println("Enter employee name");
+        name=sc.next();
+        System.out.println("Enter employee id");
+        id=sc.nextInt();
+        System.out.println("Enter salary per hour");
+        salperhour=sc.nextInt();
+        System.out.println("Enter number of working days per month");
+        monthworkday=sc.nextInt();
+        System.out.println("Enter total working hours");
+        maxHours=sc.nextInt();
+    }
+
+
+    //method for recording attendance everyday
     public void attendence(boolean a) {
         if(a){
             attendance=true;
@@ -24,6 +53,7 @@ public class Employee {
         }
     }
 
+    //calculating daily wage
     public void calDailyWage(int i){
         if(attendance){
             dailySalary=normalWorkday*salperhour;
@@ -39,15 +69,56 @@ public class Employee {
 //        MonthlySalary+=dailySalary;
 //    }
 
+    //calculating wage till condition of total working hrs or days is reached for a month
     public void calWageTillCond(){
         wageTillCon=workingHours*salperhour;
         totalWage+=wageTillCon;
     }
 
 
-    Employee(String n,int a){
-        this.name=n;
-        this.id=a;
+
+    public void compute(){
+        for(int tillHours=0,day=0;day<monthworkday && tillHours<=maxHours;day++,tillHours+=workingHours) {
+            Random r = new Random();
+            int a = (r.nextInt(1000)) % 3;
+            switch (a) {
+                case 1 ->{
+                    attendence(true);
+                    System.out.print(" Full time today: ");
+                    normalWorkday = 8;
+                    workingHours=8;
+                }
+                case 2 -> {
+                    attendence(true);
+                    System.out.print(" Part time today: ");
+                    normalWorkday = 4;
+                    workingHours=4;
+                }
+                default -> {
+                    attendence(false);
+                    workingHours=0;
+                }
+            }
+            calDailyWage(day);
+//            e1.calMonthlyWage();
+            calWageTillCond();
+            totalHours=(tillHours+workingHours);
+            System.out.println(" total working hours till now :"+totalHours);
+        }
+        System.out.println("Total wage :"+totalWage +" and total working hours :"+ totalHours);
+
     }
+
+
+
+
+
+//    Employee(String n,int a,int salperhour,int monthworkday,int maxHours){
+//        this.name=n;
+//        this.id=a;
+//        this.salperhour=salperhour;
+//        this.monthworkday=monthworkday;
+//        this.maxHours=maxHours;
+//    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,19 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public  static void ComputerWage(){
+    public  static void ComputeWage(){
         Scanner sc=new Scanner(System.in);
-
-        System.out.println("Enter employee name");
-        String name=sc.next();
-        System.out.println("Enter employee id");
-        int id=sc.nextInt();
-
-        Employee e1= new Employee(name,id);
-
-        for(int tillHours=0,day=0;day<e1.monthworkday && tillHours<=e1.maxHours;day++,tillHours+=e1.workingHours) {
-
-            Random r = new Random();
-            int a = (r.nextInt(1000)) % 3;
-
-            switch (a) {
-                case 1 ->{
-                    e1.attendence(true);
-                    System.out.print(" Full time today: ");
-                    e1.normalWorkday = 8;
-                    e1.workingHours=8;
-                }
-                case 2 -> {
-                    e1.attendence(true);
-                    System.out.print(" Part time today: ");
-                    e1.normalWorkday = 4;
-                    e1.workingHours=4;
-                }
-                default -> {
-                    e1.attendence(false);
-                    e1.workingHours=0;
-                }
-            }
-
-            e1.calDailyWage(day);
-//            e1.calMonthlyWage();
-            e1.calWageTillCond();
-
-            e1.totalHours=(tillHours+e1.workingHours);
-            System.out.println(" total working hours till now :"+e1.totalHours);
-
-        }
-        System.out.println("Total wage :"+e1.totalWage +" and total working hours :"+ e1.totalHours);
+        int i=1;
+        do {
+            Employee e1 = new Employee();
+            e1.input();
+            e1.compute();
+            System.out.println("for exit : enter 0");
+            i=sc.nextInt();
+        }while(i!=0);
     }
 
     public static void main(String[] args) {
-        ComputerWage();
+        ComputeWage();
     }
 }
